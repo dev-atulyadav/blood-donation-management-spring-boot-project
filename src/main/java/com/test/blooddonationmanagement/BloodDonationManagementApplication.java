9d093fecf07a047f635fa9da2517ddcf34869c87
dev-atulyadav <atulyadav031103@gmail.com>
2024-02-18 20:41:56 +0530

    new project

---
+++ src/main/java/com/test/blooddonationmanagement/BloodDonationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.test.blooddonationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/blooddonationmanagement/BloodDonationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.blooddonationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
